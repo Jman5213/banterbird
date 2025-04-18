59cce94b7bde16c98c410ee3665912a72755367b
Jman5213 <jman5213@outlook.com>
2025-04-18 10:40:47 -0500

    feed comes from backend

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/api/posts')
+def get_posts():
+    with open('posts.json', 'r') as f:
+        posts = json.load(f)
+
+    return jsonify(posts)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
