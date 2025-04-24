be2b5f5d7bc7861701ad113ff4a1af93b7146f1b
Jman5213 <jman5213@outlook.com>
2025-04-24 09:50:19 -0500

    submit post improvements
    submit post now updates the feed immediately

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,15 @@
 const username = "admin";
 
-function renderPost(post) {
+function renderPost(post, isNew=false) {
     const template = document.getElementById("post-template").content.cloneNode(true);
     template.querySelector(".username").innerText = post.username;
     template.querySelector(".message").innerText = post.message;
-    document.getElementById("feed").appendChild(template);
+
+    if (isNew){
+        document.getElementById("feed").prepend(template);
+    } else {
+        document.getElementById("feed").appendChild(template);
+    }
 }
 
 async function submitPost() {
@@ -20,7 +25,11 @@ async function submitPost() {
                 message,
             }),
         });
-    } catch {
+        if (response.ok) {
+            renderPost({username, message}, true);
+            document.getElementById("postInput").value = "";
+        }
+    } catch (error) {
         console.error("⚠️ Error! ⚠️ ", error);
     }
 }
